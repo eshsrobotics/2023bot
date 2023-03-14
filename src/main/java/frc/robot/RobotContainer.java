d8c31fd509c479eccc6b59cfce4dc88ea948eb0b
uakotaobi <uche.robotics@akotaobi.org>
2023-03-13 19:04:32 -0700

    Temporary commit; lab closing.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import java.util.Map;
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
@@ -13,6 +16,8 @@ import frc.robot.subsystems.VroomSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
+import edu.wpi.first.wpilibj2.command.SelectCommand;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -40,10 +45,46 @@ public class RobotContainer {
     inputSubsystem = new InputSubsystem(shuffleboardDebug);
     driveSubsystem  = new VroomSubsystem(inputSubsystem, shuffleboardDebug);
     
+    // Create a command that repeatedly performs any one of the following until
+    // that particular action has ended:
+    //
+    // 1. If the user hits the reset button, returns the arm to its home
+    //    position.
+    // 2. If the user hits the low goal button, moves the arm to the low goal
+    //    (x, y) position.
+    // 3. If the user hits the high goal button, moves the arm to the high goal
+    //    (x, y) position.
+    // 4. If the user has done none of the above, spontaneously operate the arm
+    //    in full manual mode.
+    //
+    // These commands are all InstantCommands, but the first three have a much
+    // longer duration than the last one.
     teleopCompositeCommand = new RepeatCommand(new InstantCommand(() -> {
       // This repeat command dispatches other commands and then exits
+      final int RESET_STATE = 1;
+      final int LOW_STATE = 2;
+      final int HIGH_STATE = 3;
+      final int MANUAL_OPERATION_FOR_NOW = 4;
       
+      Command resetCommand = new WaitCommand(2.0)
+        .beforeStarting(() -> System.out.printf("Started reset\n"))
+        .andThen(() -> System.out.printf("Ended reset\n"));
+      
+      Command lowCommand = new WaitCommand(5.0)
+        .beforeStarting(() -> System.out.printf("Moving to low goal\n"))
+        .andThen(() -> System.out.printf("Done moving to low goal\n"));
+      
+      final Map<Integer, Command> commandMap = 
+        Map.of(RESET_STATE, resetCommand,
+               LOW_STATE, lowCommand);
 
+      // Choose what arm state to be in based on human input.
+      Supplier<Integer> stateSelector = () -> {
+        // TODO: Finish this.
+        return MANUAL_OPERATION_FOR_NOW;
+      };
+
+      
     }, inputSubsystem));
   
   }
